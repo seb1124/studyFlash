4bad9104d45020980dd27b7277d088f5f4d04837
seb1124 <sebastian112403@gmail.com>
2024-10-05 17:21:36 -0400

    update

--- scripts/flash.js
+++ scripts/flash.js
@@ -40,7 +40,6 @@ document.querySelector("#forward-arrow").addEventListener("click", () => {
     }
 });
 
-opneaiApiHandler("this is sami he has green hair and red eyebrows. Mark drives a bugatti.");
 // Load stored text on page load
 async function loadStoredText() {
     const storedText = localStorage.getItem("pdfText");  // Get the stored text from local storage
